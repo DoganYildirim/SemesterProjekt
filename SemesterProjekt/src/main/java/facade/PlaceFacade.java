a7234c942614dda62c6f0ca2b7a27b32baba7df5
Gozzah <rune.halvor@yahoo.dk>
2017-11-21 14:47:20 +0100

    Update PlaceFacade.java
    Added create place

--- SemesterProjekt/src/main/java/facade/PlaceFacade.java
+++ SemesterProjekt/src/main/java/facade/PlaceFacade.java
@@ -5,10 +5,66 @@
  */
 package facade;
 
+import entity.Place;
+import entity.Zip;
+import enums.Pets;
+import enums.Rented;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author Peter Riis
  */
 public class PlaceFacade {
+
+     public PlaceFacade(EntityManagerFactory emf)
+    {
+        this.emf = emf;
+    }
+    
+   EntityManagerFactory emf = Persistence.createEntityManagerFactory("PU");
+    EntityManagerFactory emfUpdate = Persistence.createEntityManagerFactory("PU");
+    EntityManager em = emf.createEntityManager();
+    EntityManager emUpdate = emf.createEntityManager();
+
+    public EntityManager getEntityManagerUpdate()
+    {
+        return emfUpdate.createEntityManager();
+    }
+
+    public EntityManager getEntityManager()
+    {
+        return emf.createEntityManager();
+    }
+    
+    public void CreatePlace(String owner, String adress, String numberOfRooms, String numberOfPersons, String description, Pets pets, Rented rented, Zip zip){
+        
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        
+        Place place= new Place();
+        
+        place.setOwner(owner);
+        place.setAdress(adress);
+        place.setNumberOfRooms(numberOfRooms);
+        place.setNumberOfPersons(numberOfPersons);
+        place.setDescription(description);
+        place.setPets(pets);
+        place.setRented(rented);
+        place.setZip(zip);
+        
+        em.persist(place);
+        em.getTransaction().commit();
+        em.close();
+        
+        
+        
+        
+        
+        
+        
+    }
     
 }
