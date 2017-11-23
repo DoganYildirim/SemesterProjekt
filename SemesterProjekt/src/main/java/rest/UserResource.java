d5cf35bb1b663d9b0821c6d29cfaec4d935f0ff4
pedskon <peter-riis@hotmail.com>
2017-11-23 15:27:33 +0100

    Update UserResource.java

--- SemesterProjekt/src/main/java/rest/UserResource.java
+++ SemesterProjekt/src/main/java/rest/UserResource.java
@@ -11,16 +11,19 @@ import java.util.ArrayList;
 import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import javax.persistence.EntityManager;
+import java.io.IOException;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -47,8 +50,8 @@ public class UserResource {
      * Retrieves representation of an instance of rest.UserResource
      * @return an instance of java.lang.String
      */
-    @Path("getUsers")
     @GET
+    @Path("getUsers")
     @Produces(MediaType.APPLICATION_JSON)
     public String getAllUsers() {
         
@@ -59,13 +62,64 @@ public class UserResource {
     return result;
     
     }
-
+    
+    @GET
+    @Path("getUserByID")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getUserByID(@PathParam("id") int id) {
+        
+    UserFacade uf = new UserFacade(emf);
+    User user = new User();
+    user = uf.getUserByID(id);
+    String result = gson.toJson(user);
+    return result;
+    }
+    
+    @POST
+    @Path("createUser")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String createUser(String content) throws IOException
+    {
+        UserFacade uf = new UserFacade(emf);
+        User u = gson.fromJson(content, User.class);
+        
+        uf.createUser(
+        u.getFirstName(), u.getLastName(),
+        u.getUserName(), u.getPassword(), u.getEmail(),
+        u.getPhoneNumber(), u.getProfilePic()
+        );
+        
+        return gson.toJson(u);
+    }
+    
+    @DELETE
+    @Path("\"/{id}\"")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public void deleteUser(@PathParam("id")int id){
+        
+        UserFacade uf = new UserFacade(emf);
+        uf.deleteUser(id);
+    }
+    
+    
     /**
      * PUT method for updating or creating an instance of UserResource
      * @param content representation for the resource
      */
     @PUT
+    @Path("\"/{id}\"")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
+    public void putJson(@PathParam("id")int id) {
+
+        UserFacade uf = new UserFacade(emf);
+        User u = uf.getUserByID(id);
+        
+        uf.editUser(
+        0, u.getFirstName(), u.getLastName(),
+        u.getUserName(), u.getPassword(), u.getEmail(),
+        u.getPhoneNumber(), u.getProfilePic()
+        );
     }
 }
