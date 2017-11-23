6b335acea89feba14ebf95c504ace6cb5be6d4e6
pedskon <peter-riis@hotmail.com>
2017-11-23 12:39:03 +0100

    Update User.java

--- SemesterProjekt/src/main/java/entity/User.java
+++ SemesterProjekt/src/main/java/entity/User.java
@@ -24,6 +24,11 @@ public class User implements Serializable {
     public User() {
     }
 
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
     
     
     @ManyToOne
