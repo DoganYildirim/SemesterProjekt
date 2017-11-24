660841991667850d6c80662a3ad45fe0b1aa163d
DoganYildirim <24220738+DoganYildirim@users.noreply.github.com>
2017-11-24 10:55:13 +0100

    Update PlaceFacade.java

--- SemesterProjekt/src/main/java/facade/PlaceFacade.java
+++ SemesterProjekt/src/main/java/facade/PlaceFacade.java
@@ -19,113 +19,138 @@ import javax.persistence.Query;
  *
  * @author Peter Riis
  */
-public class PlaceFacade {
+public class PlaceFacade
+{
 
-     public PlaceFacade(EntityManagerFactory emf){
+    public PlaceFacade(EntityManagerFactory emf)
+    {
         this.emf = emf;
     }
-    
+
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("PU");
     EntityManagerFactory emfUpdate = Persistence.createEntityManagerFactory("PU");
     EntityManager em = emf.createEntityManager();
     EntityManager emUpdate = emf.createEntityManager();
 
-    public EntityManager getEntityManagerUpdate(){
+    public EntityManager getEntityManagerUpdate()
+    {
         return emfUpdate.createEntityManager();
     }
 
-    public EntityManager getEntityManager(){
+    public EntityManager getEntityManager()
+    {
         return emf.createEntityManager();
     }
-    
-    
-    
-    public void createPlacetwo(String owner, String adress, String numberOfRooms, String numberOfPersons, String description){
-        try{
-            
-        EntityManager em = getEntityManager();
-        em.getTransaction().begin();
-        
-        Place place= new Place();
-        
-        place.setOwner(owner);
-        place.setAdress(adress);
-        place.setNumberOfRooms(numberOfRooms);
-        place.setNumberOfPersons(numberOfPersons);
-        place.setDescription(description);
-        
-       em.persist(place);
-        em.getTransaction().commit();
-        }finally{
-           em.close();
+
+    public void createPlacetwo(String owner, String adress, String numberOfRooms, String numberOfPersons, String description)
+    {
+        try
+        {
+
+            EntityManager em = getEntityManager();
+            em.getTransaction().begin();
+
+            Place place = new Place();
+
+            place.setOwner(owner);
+            place.setAdress(adress);
+            place.setNumberOfRooms(numberOfRooms);
+            place.setNumberOfPersons(numberOfPersons);
+            place.setDescription(description);
+
+            em.persist(place);
+            em.getTransaction().commit();
+        } finally
+        {
+            em.close();
         }
 
     }
-    
-    
-    
-    public void createPlace(String owner, String adress, String numberOfRooms, String numberOfPersons, String description, Pets pets, Rented rented, Zip zip){
-        try{
-            
-        EntityManager em = getEntityManager();
-        em.getTransaction().begin();
-        
-        Place place= new Place();
-        
-        place.setOwner(owner);
-        place.setAdress(adress);
-        place.setNumberOfRooms(numberOfRooms);
-        place.setNumberOfPersons(numberOfPersons);
-        place.setDescription(description);
-        place.setPets(pets);
-        place.setRented(rented);
-        place.setZip(zip);
-        
-       em.persist(place);
-        em.getTransaction().commit();
-        }finally{
-           em.close();
+
+    public void createPlace(String owner, String adress, String numberOfRooms, String numberOfPersons, String description, Pets pets, Rented rented, Zip zip)
+    {
+        try
+        {
+
+            EntityManager em = getEntityManager();
+            em.getTransaction().begin();
+
+            Place place = new Place();
+
+            place.setOwner(owner);
+            place.setAdress(adress);
+            place.setNumberOfRooms(numberOfRooms);
+            place.setNumberOfPersons(numberOfPersons);
+            place.setDescription(description);
+            place.setPets(pets);
+            place.setRented(rented);
+            place.setZip(zip);
+
+            em.persist(place);
+            em.getTransaction().commit();
+        } finally
+        {
+            em.close();
         }
 
     }
-    
-    public List getAllPlaces(){
-        
+
+    public List getAllPlaces()
+    {
+
         EntityManager em = getEntityManager();
         em.getTransaction().begin();
         Query query = em.createQuery("Select p from Place as p", Place.class);
-        List <Place> places = query.getResultList();
+        List<Place> places = query.getResultList();
         return places;
-        
+
     }
+
+    public Place getPlaceByName(String name)
+    {
+        EntityManager em = emf.createEntityManager(); 
     
-      public void deletePlace(long id){
+        try
+        {
+            em.getTransaction().begin();
+            Place p = em.find(Place.class, name);
+            em.getTransaction().commit();
+            return p;
+        } finally
+        {
+            em.close();
+        }
+    }
+
+    public void deletePlace(long id)
+    {
         EntityManager em = getEntityManager();
-        
+
         em.getTransaction().begin();
         Query query = em.createQuery("DELETE FROM  Place p Where p.id = :ID");
         query.setParameter("ID", id).executeUpdate();
-        
+
         em.getTransaction().commit();
         em.close();
     }
-    
-    public void editPlace(long id,String owner, String adress, String numberOfRooms, String numberOfPersons, String description, Pets pets, Rented rented, Zip zip){
-       EntityManager em = getEntityManager();
-       em.getTransaction().begin();
-       Place placeToEdit = em.find(Place.class, id);
-       placeToEdit.setOwner(owner);
-       placeToEdit.setAdress(adress);
-       placeToEdit.setNumberOfRooms(numberOfRooms);
-       placeToEdit.setNumberOfPersons(numberOfPersons);
-       placeToEdit.setDescription(description);
-       placeToEdit.setPets(pets);
-       placeToEdit.setRented(rented);
-       placeToEdit.setZip(zip);
-       
+
+    public void editPlace(long id, String owner, String adress, String numberOfRooms, String numberOfPersons, String description, Pets pets, Rented rented, Zip zip)
+    {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        Place placeToEdit = em.find(Place.class, id);
+        placeToEdit.setOwner(owner);
+        placeToEdit.setAdress(adress);
+        placeToEdit.setNumberOfRooms(numberOfRooms);
+        placeToEdit.setNumberOfPersons(numberOfPersons);
+        placeToEdit.setDescription(description);
+        placeToEdit.setPets(pets);
+        placeToEdit.setRented(rented);
+        placeToEdit.setZip(zip);
+
         em.getTransaction().commit();
         em.close();
-          
+
     }
-    
+
 }
