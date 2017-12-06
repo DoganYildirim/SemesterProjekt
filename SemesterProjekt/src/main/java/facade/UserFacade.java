195efafdb453ed4abaa71d705278f1682562e1ee
pedskon <peter-riis@hotmail.com>
2017-12-06 15:57:45 +0100

    Update UserFacade.java

--- SemesterProjekt/src/main/java/facade/UserFacade.java
+++ SemesterProjekt/src/main/java/facade/UserFacade.java
@@ -5,15 +5,11 @@
  */
 package facade;
 
-import entity.Place;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import entity.User;
-import entity.Zip;
-import enums.Pets;
-import enums.Rented;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -43,26 +39,38 @@ public class UserFacade{
         return emf.createEntityManager();
     }
 
-    public void createUser(String firstName, String lastName, String username, String password, String email, String phoneNo, byte[] picInBytes) throws FileNotFoundException, IOException {
+    public void createUser(String firstName, String lastName, String username, String password, String email, String phoneNumber) throws FileNotFoundException, IOException {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        // byte[] picInBytes skal i construtoren hvis billede er på.
+        User user = new User();
+        
+        
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setUserName(username);
+        user.setPassword(password);
+        user.setEmail(email);
+        user.setPhoneNumber(phoneNumber);
+
+//        File file = new File("C:\\Users\\Peter Riis\\Desktop\\maymays\\Doggos\\hqdefault.jpg");
+//        picInBytes = new byte[(int) file.length()];
+//        FileInputStream fileInputStream = new FileInputStream(file);
+//        fileInputStream.read(picInBytes);
+//        fileInputStream.close();
+//        user.setProfilePic(picInBytes);
+
+        em.persist(user);
+        em.getTransaction().commit();
+        em.close();
+    }
+    public void createUser(String firstName) throws FileNotFoundException, IOException {
         EntityManager em = getEntityManager();
         em.getTransaction().begin();
 
         User user = new User();
 
         user.setFirstName(firstName);
-        user.setLastName(lastName);
-        user.setUserName(username);
-        user.setPassword(password);
-        user.setEmail(email);
-        user.setPhoneNumber(phoneNo);
-
-        File file = new File("C:\\Users\\Peter Riis\\Desktop\\maymays\\Doggos\\hqdefault.jpg");
-        picInBytes = new byte[(int) file.length()];
-        FileInputStream fileInputStream = new FileInputStream(file);
-        fileInputStream.read(picInBytes);
-        fileInputStream.close();
-        user.setProfilePic(picInBytes);
-
         em.persist(user);
         em.getTransaction().commit();
         em.close();
@@ -118,6 +126,17 @@ public class UserFacade{
        user.setEmail(email);
        user.setPhoneNumber(phoneNo);
        user.setProfilePic(picInBytes);
+       
+        em.getTransaction().commit();
+        em.close();
+          
+    }
+    public void editUserTest(long id, String firstName){
+       EntityManager em = getEntityManager();
+       em.getTransaction().begin();
+       User user = em.find(User.class, id);
+       user.setFirstName(firstName);
+
        
         em.getTransaction().commit();
         em.close();
